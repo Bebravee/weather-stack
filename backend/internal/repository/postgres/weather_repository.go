c04eb27cc1f0aecdbb6e8343d200a044e3f63e96
AArtemBashta <60484170+StranaGedon-ru@users.noreply.github.com>
2025-11-09 21:51:14 +0300

    Update weather_repository.go

--- backend/internal/repository/postgres/weather_repository.go
+++ backend/internal/repository/postgres/weather_repository.go
@@ -15,16 +15,36 @@ func NewWetherRepository(db *gorm.DB) *WetherRepository {
 	return &WetherRepository{db: db}
 }
 
-func (r *WetherRepository) CreateWeatherRequest(ctx context.Context, new *domain.WeatherRequestEntity) error {
-	return r.db.WithContext(ctx).Table("weather_requests").Create(new).Error
+func (r *WetherRepository) CreateWeatherRequest(ctx context.Context, new *domain.WeatherEntity) error {
+	return r.db.WithContext(ctx).Table("weather").Create(new).Error
 }
 
 func (r *WetherRepository) GetWeatherByCity(ctx context.Context, city string) ([]*domain.WeatherEntity, error) {
 	weather := []*domain.WeatherEntity{}
 
-	if err := r.db.WithContext(ctx).Table("weather_requests").Find(&weather).Error; err != nil {
+	if err := r.db.WithContext(ctx).Table("weather").Where("city_id = ?", city).First(weather).Error; err != nil {
 		return nil, err
 	}
 
 	return weather, nil
+}
+
+func (r *WetherRepository) GetClothesByComb(ctx context.Context, id int) ([]*domain.ClothesEntity, error) {
+	clothes := []*domain.ClothesEntity{}
+
+	if err := r.db.WithContext(ctx).Table("clothes").Where("id = ?", id).First(clothes).Error; err != nil {
+		return nil, err
+	}
+
+	return clothes, nil
+}
+
+func (r *WetherRepository) GetNewsByCity(ctx context.Context, city string) ([]*domain.WeatherEntity, error) {
+	news := []*domain.NewsEntity{}
+
+	if err := r.db.WithContext(ctx).Table("news").Where("city_id = ?", city).First(news).Error; err != nil {
+		return nil, err
+	}
+
+	return news, nil
 }
