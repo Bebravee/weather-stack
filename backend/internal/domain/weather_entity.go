c03005777c0637cba6dd0b7551f12363fde74985
AArtemBashta <60484170+StranaGedon-ru@users.noreply.github.com>
2025-11-09 19:45:18 +0300

    Update weather_entity.go

--- backend/internal/domain/weather_entity.go
+++ backend/internal/domain/weather_entity.go
@@ -5,9 +5,12 @@ import (
 )
 
 type WeatherRequestEntity struct {
-	ID          int64     `json:"id" gorm:"id"`
-	UserID      UserID    `json:"user_id" gorm:"user_id"`
+	CITY_ID     string    `json:"city_id" gorm:"city_id"`
 	Temperature float64   `json:"temperature" gorm:"temperature"`
-	Clothing    string    `json:"clothing" gorm:"clothing"`
+	feels_like  float64   `json:"feels" gorm:"feels"`
+	Description string    `json:"description" gorm:"description"`
+	Humidity    float64   `json:"humidity" gorm:"humidity"`
+	Pressure    float64   `json:"pressure" gorm:"pressure"`
+	Wind_speed  float64   `json:"wind_speed" gorm:"wind_speed"`
 	CreatedAt   time.Time `json:"created_at" db:"created_at"`
 }
