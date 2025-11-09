ce6bd1f794317a814c6b90f2db3c6ab1bab32cb0
AArtemBashta <60484170+StranaGedon-ru@users.noreply.github.com>
2025-11-09 19:19:41 +0300

    Update 000002_weather_requests.up.sql
    Now db is entertaining our needs.

--- backend/migrations/000002_weather_requests.up.sql
+++ backend/migrations/000002_weather_requests.up.sql
@@ -1,8 +1,11 @@
 CREATE TABLE IF NOT EXISTS weather_requests(
-	id          SERIAL       PRIMARY KEY,
-	user_id     UUID         REFERENCES users,
+	city_id     VARCHAR(30)  PRIMARY KEY,
 	temperature NUMERIC(5,2) NOT NULL,
-	clothing    VARCHAR(50)  NOT NULL,
+	feels_like  NUMERIC(5,2) NOT NULL,
+	description VARCHAR(50)  NOT NULL,
+	humidity	NUMERIC(5,2) NOT NULL,
+	pressure 	NUMERIC(5,2) NOT NULL,
+	wind_speed  NUMERIC(5,2) NOT NULL,
 	
 	created_at  TIMESTAMP    NOT NULL DEFAULT now()
 );
